feec94eab153d66f41206791589fd05931f29d51
wojtek746 <Wojtek7462@gmail.com>
2022-11-09 20:11:40 +0100

    building system

--- Wojtkowice/Assets/Scenes/buildSystem.cs
+++ Wojtkowice/Assets/Scenes/buildSystem.cs
@@ -5,17 +5,46 @@ using UnityEngine;
 public class buildSystem : MonoBehaviour
 {
     private blockSystem blockSys;
-    private int currentID = 0;
     private Block currentBlock;
     private GameObject blockTemplate;
     private SpriteRenderer currentRend;
+    [SerializeField]
+    private LayerMask solidNoBuildLayer;
+    [SerializeField]
+    private LayerMask backingNoBuildLayer;
 
     private void Awake()
     {
         blockSys = GetComponent<blockSystem>();
-        currentBlock = blockSys.allBlocks[currentID];
+        Creating("Dungon101_Tileset_5_solid", -3, -3);
+        Creating("Dungon101_Tileset_4_solid", -2, -2);
+        Creating("Dungon101_Tileset_6_solid", -1, 1);
+        Creating("Dungon101_Tileset_5_solid", 2, 4);
+    }
+
+    private void Creating(string id, float x, float y)
+    {
+        for(int i = 0; i < blockSys.allBlocks.Length; i++)
+        {
+            if(blockSys.allBlocks[i].name == id)
+            {
+                currentBlock = blockSys.allBlocks[i];
+                break; 
+            }
+        }
         blockTemplate = new GameObject("CurrentBlockTemplate");
         currentRend = blockTemplate.AddComponent<SpriteRenderer>();
-        currentRend.sprite = currentBlock.sprite; 
+        currentRend.sprite = currentBlock.sprite;
+        blockTemplate.transform.position = new Vector2(x, y);
+        int layer; 
+        if(currentBlock.isSolid)
+        {
+            layer = LayerMask.NameToLayer("Solid Block");
+        }
+        else
+        {
+            layer = LayerMask.NameToLayer("Backing Block");
+        }
+        blockTemplate.layer = layer;
     }
 }
