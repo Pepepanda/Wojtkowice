55409d320a68e1ce8413e743d512a696d8a04ec3
wojtek746 <Wojtek7462@gmail.com>
2022-11-16 17:02:42 +0100

    Working buildSystem2.cs

--- Wojtkowice/Assets/Scenes/buildSystem2.cs
+++ Wojtkowice/Assets/Scenes/buildSystem2.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class buildSystem2 : MonoBehaviour
 {
+    private System.Random rand = new System.Random();
+
     int numberRoomsLUDR = 1; 
     [SerializeField]
     GameObject[] roomsLUDR;
@@ -13,7 +15,7 @@ public class buildSystem2 : MonoBehaviour
     int numberRoomsLUR = 1;
     [SerializeField]
     GameObject[] roomsLUR;
-    int numberRoomsLUDR = 1;
+    int numberRoomsLDR = 1;
     [SerializeField]
     GameObject[] roomsLDR;
     int numberRoomsUDR = 1;
@@ -32,16 +34,102 @@ public class buildSystem2 : MonoBehaviour
     [SerializeField]
     GameObject[] roomsDR;
 
-    void awake()
+    void Awake()
     {
-
+        //createRoom("LUD", 0, 0);
+        //createRoom("LUR", 18, 0);
+        //createRoom("LDR", 0, 10);
+        //createRoom("UDR", 18, 10);
+        //createRoom("LU", 0, 0);
+        //createRoom("UR", 18, 0);
+        //createRoom("LD", 0, 10);
+        //createRoom("DR", 18, 10);
+        createHause(8, 7, 0, 0); 
     }
 
-    void createRoom(string nameOfRoom, int StartX, int StartY)
+    void createHause(int width, int height, int StartX, int StartY)
     {
-        if(nameOfRoom == "LUDR")
+        for(int x = 0; x < width; x++)
         {
-            
+            for( int y = 0; y < height; y++)
+            {
+                if (x == 0 && y == 0)
+                {
+                    createRoom("UR", StartX, StartY);
+                }
+                else if (x == 0 && y == height - 1)
+                {
+                    createRoom("DR", StartX, StartY + (y * 10));
+                }
+                else if (x == width - 1 && y == 0)
+                {
+                    createRoom("LU", StartX + (x * 18), StartY);
+                }
+                else if (x == width - 1 && y == height - 1)
+                {
+                    createRoom("LD", StartX + (x * 18), StartY + (y * 10));
+                }
+                else if (x == 0)
+                {
+                    createRoom("UDR", StartX, StartY + (y * 10));
+                }
+                else if (x == width - 1)
+                {
+                    createRoom("LUD", StartX + (x * 18), StartY + (y * 10));
+                }
+                else if (y == 0)
+                {
+                    createRoom("LUR", StartX + (x * 18), StartY);
+                }
+                else if (y == width - 1)
+                {
+                    createRoom("LDR", StartX + (x * 18), StartY + (y * 10));
+                }
+                else
+                {
+                    createRoom("LUDR", StartX + (x * 18), StartY + (y * 10)); 
+                }
+            }
+        }
+    }
+
+    void createRoom(string nameOfRoom, int x, int y)
+    {
+        if (nameOfRoom == "LUDR")
+        {
+            Instantiate(roomsLUDR[rand.Next(numberRoomsLUDR)], new Vector2(x, y), Quaternion.identity);
+        }
+        else if (nameOfRoom == "LUD")
+        {
+            Instantiate(roomsLUD[rand.Next(numberRoomsLUD)], new Vector2(x, y), Quaternion.identity);
+        }
+        else if (nameOfRoom == "LUR")
+        {
+            Instantiate(roomsLUR[rand.Next(numberRoomsLUR)], new Vector2(x, y), Quaternion.identity);
+        }
+        else if (nameOfRoom == "LDR")
+        {
+            Instantiate(roomsLDR[rand.Next(numberRoomsLDR)], new Vector2(x, y), Quaternion.identity);
+        }
+        else if (nameOfRoom == "UDR")
+        {
+            Instantiate(roomsUDR[rand.Next(numberRoomsUDR)], new Vector2(x, y), Quaternion.identity);
+        }
+        else if (nameOfRoom == "LU")
+        {
+            Instantiate(roomsLU[rand.Next(numberRoomsLU)], new Vector2(x, y), Quaternion.identity);
+        }
+        else if (nameOfRoom == "UR")
+        {
+            Instantiate(roomsUR[rand.Next(numberRoomsUR)], new Vector2(x, y), Quaternion.identity);
+        }
+        else if (nameOfRoom == "LD")
+        {
+            Instantiate(roomsLD[rand.Next(numberRoomsLD)], new Vector2(x, y), Quaternion.identity);
+        }
+        else if (nameOfRoom == "DR")
+        {
+            Instantiate(roomsDR[rand.Next(numberRoomsDR)], new Vector2(x, y), Quaternion.identity);
         }
     }
 }
