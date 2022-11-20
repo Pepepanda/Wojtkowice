68dfc3ccb425b72b1cc9ab28f6d875035eb64e09
Pepepanda <michal.danielczok@gmail.com>
2022-11-20 11:45:44 +0100

    EnemyMovment

--- Wojtkowice/Assets/Player/PlayerHealth.cs
+++ Wojtkowice/Assets/Player/PlayerHealth.cs
@@ -11,7 +11,7 @@ public class PlayerHealth : MonoBehaviour
     void Start()
     {
         health = maxHealth;
-        //healthBar.SetHealth(maxHealth);
+        healthBar.SetHealth(maxHealth);
     }
 
     //how much damage the player takes 
@@ -28,6 +28,6 @@ public class PlayerHealth : MonoBehaviour
     public void GetHealth()
     {
         health +=5;
-        //healthBar.SetHealth(health);
+        healthBar.SetHealth(health);
     }
 }
