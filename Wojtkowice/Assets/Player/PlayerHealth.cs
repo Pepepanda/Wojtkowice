bafb58d0bf05a409329a75ac3dfa33b4c7e1310b
Koziom <kziomeczkowy@gmail.com>
2023-05-08 16:21:14 +0200

    Naprawa przeciwnika
    Co-Authored-By: wojtek746 <97599947+wojtek746@users.noreply.github.com>

--- Wojtkowice/Assets/Player/PlayerHealth.cs
+++ Wojtkowice/Assets/Player/PlayerHealth.cs
@@ -10,6 +10,7 @@ public class PlayerHealth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        healthBar = GameObject.Find("Health").GetComponent<HealthBar>();
         health = maxHealth;
         healthBar.SetHealth(maxHealth);
     }
